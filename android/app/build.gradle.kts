e5dff4e42c56468dfcbdc4cb75f3a9b3ca504b41
machine <vladislav.kerimov.work@mail.ru>
2026-07-16 22:48:58 +0500

    feat: updated compile version and updated gradle properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.app_mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34 
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,20 +15,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        applicationId = "com.machine.app_mobile"
+        minSdk = 21 
+        targetSdk = 34 
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
